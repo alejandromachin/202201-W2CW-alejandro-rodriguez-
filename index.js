2ae61807da3e275d279cf6140c380b35f71ffd33
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-21 20:57:22 +0100

    changed table in html to one generated with js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const rows = 20;
+const columns = 20;
 
+function gameboard() {
+  const world = document.querySelector(".world");
+  const table = document.createElement("table");
+
+  for (let i = 0; i < rows; i++) {
+    const tr = document.createElement("tr");
+
+    for (let j = 0; j < columns; j++) {
+      const cells = document.createElement("cells");
+
+      tr.appendChild(cells);
+    }
+    table.appendChild(tr);
+  }
+  world.appendChild(table);
+}
+
+gameboard();
