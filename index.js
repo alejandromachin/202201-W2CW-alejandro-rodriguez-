18ca410963afa3dd9edbf25fa90749381ca1f4fc
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-22 00:26:01 +0100

    created the function newArray

--- index.js
+++ index.js
@@ -22,12 +22,40 @@ window.onload = () => {
   gameboard();
 };
 
-const numberOfCells = 400;
-
+const numberOfCells = 25;
 const newArray = () => {
   const myArray = [];
-
   while (myArray.length < numberOfCells) {
-    myArray.push(true);
+    myArray.push(false);
   }
+
+  const linea1 = [];
+  for (let k = 0; k < 5; k++) {
+    linea1.push(myArray[k]);
+  }
+
+  const linea2 = [];
+  for (let k = 5; k < 10; k++) {
+    linea2.push(myArray[k]);
+  }
+
+  const linea3 = [];
+  for (let k = 10; k < 15; k++) {
+    linea3.push(myArray[k]);
+  }
+  const linea4 = [];
+  for (let k = 15; k < 20; k++) {
+    linea4.push(myArray[k]);
+  }
+  const linea5 = [];
+  for (let k = 20; k < 25; k++) {
+    linea5.push(myArray[k]);
+  }
+
+  const board = [linea1, linea2, linea3, linea4, linea5];
+
+  return board;
 };
+
+const prueba = newArray();
+console.log(prueba);
---
+++ tests/functions.test.js
@@ -0,0 +1,62 @@
+const numberOfCells = 25;
+const newArray = () => {
+  const myArray = [];
+  while (myArray.length < numberOfCells) {
+    myArray.push(false);
+  }
+
+  const linea1 = [];
+  for (let k = 0; k < 5; k++) {
+    linea1.push(myArray[k]);
+  }
+
+  const linea2 = [];
+  for (let k = 5; k < 10; k++) {
+    linea2.push(myArray[k]);
+  }
+
+  const linea3 = [];
+  for (let k = 10; k < 15; k++) {
+    linea3.push(myArray[k]);
+  }
+  const linea4 = [];
+  for (let k = 15; k < 20; k++) {
+    linea4.push(myArray[k]);
+  }
+  const linea5 = [];
+  for (let k = 20; k < 25; k++) {
+    linea5.push(myArray[k]);
+  }
+
+  const board = [linea1, linea2, linea3, linea4, linea5];
+
+  return board;
+};
+
+describe("Given a function newArray()", () => {
+  describe("When it is called", () => {
+    test("Then it should return an array of 5 false values", () => {
+      const expectedArray = [false, false, false, false, false];
+      const numberOfCells = 5;
+      const newArrayReturned = newArray();
+
+      expect(newArrayReturned).toEqual(expectedArray);
+    });
+  });
+
+  describe("When it is called", () => {
+    test("Then it should return a bidimentional array of 5*5 false values", () => {
+      const expectedArray = [
+        [false, false, false, false, false],
+        [false, false, false, false, false],
+        [false, false, false, false, false],
+        [false, false, false, false, false],
+        [false, false, false, false, false],
+      ];
+
+      const newArrayReturned = newArray();
+
+      expect(newArrayReturned).toEqual(expectedArray);
+    });
+  });
+});
