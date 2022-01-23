47c9b78fd65a263d23f4cb4c4c5ffe45c0ac63cc
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-23 16:48:28 +0100

    added id to restart div and js function to show play section

--- index.js
+++ index.js
@@ -223,6 +223,12 @@ start.addEventListener("click", () => {
   loop();
 });
 
+const replay = document.getElementById("replay");
+replay.addEventListener("click", () => {
+  document.querySelector(".replay").style.display = "none";
+  document.querySelector(".play").style.display = "flex";
+});
+
 // Any live cell with two or three live neighbours survives.
 // Any dead cell with three live neighbours becomes a live cell.
 // All other live cells die in the next generation. Similarly, all other dead cells stay dead.
